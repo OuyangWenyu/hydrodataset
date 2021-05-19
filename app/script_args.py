af2265fee3db9d97b26431e65b94018b5a952aa3
Wenyu Ouyang <hust2014owen@gmail.com>
2021-05-19 13:45:15 +0800

    batch download shp and daymet

---
+++ app/script_args.py
@@ -0,0 +1,24 @@
+#################################################
+# to test if the arg-parse commands are correct
+import argparse
+import os
+import sys
+
+sys.path.append("..")
+import definitions
+
+
+def main(args):
+    """
+    Main function which is called from the command line. Entrypoint for training all ML models.
+    """
+    print("the years: ", list(range(int(args.year_range[0]), int(args.year_range[1]))))
+
+
+# python script_args.py --year_range 1990 2011
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Download Daymet within the boundary of each basin in CAMELS')
+    parser.add_argument('--year_range', dest='year_range', help='The start and end years (right open interval)',
+                        default=[1980, 2021], nargs='+')
+    the_args = parser.parse_args()
+    main(the_args)
