cfb7017c44764a2445ce8e6d9f01ee76c3704723
owen <hust2014owen@gmail.com>
2021-08-04 11:07:16 +0800

    more testing code for geo boundary

--- test/test_downloaded_data.py
+++ test/test_downloaded_data.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 import xarray as xr
+import rasterio.features as rio_features
+import pygeoutils as geoutils
 
 import definitions
 from hydrobench.data.data_camels import Camels
@@ -69,6 +71,65 @@ class MyTestCase(unittest.TestCase):
         print(gb_east < df_east)
         print(gb_north < df_north)
         print(gb_south > df_south)
+
+    def test3_trans_to_rectangle(self):
+        basin_id = "01013500"
+        camels_shp_file = self.camels.dataset_description["CAMELS_BASINS_SHP_FILE"]
+        camels_shp = gpd.read_file(camels_shp_file)
+        # transform the geographic coordinates to wgs84 i.e. epsg4326  it seems NAD83 is equal to WGS1984 in geopandas
+        camels_shp_epsg4326 = camels_shp.to_crs(epsg=4326)
+        geometry = camels_shp_epsg4326[camels_shp_epsg4326["hru_id"] == int(basin_id)].geometry.item()
+        save_path = os.path.join(self.save_dir, basin_id + "_camels.shp")
+        camels_shp_epsg4326[camels_shp_epsg4326["hru_id"] == int(basin_id)].geometry.to_file(save_path)
+
+        read_path = os.path.join(self.save_dir, basin_id + "_2000_01_01-03_from_urls.nc")
+        ds = xr.open_dataset(read_path)
+        ds_dims = ("y", "x")
+        transform, width, height = geoutils.pygeoutils._get_transform(ds, ds_dims)
+        _geometry = geoutils.pygeoutils._geo2polygon(geometry, "epsg:4326", ds.crs)
+
+        _mask = rio_features.geometry_mask([_geometry], (height, width), transform, invert=True)
+        # x - column, y - row
+        y_idx, x_idx = np.where(_mask)
+        y_idx_min = y_idx.min()
+        y_idx_max = y_idx.max()
+        x_idx_min = x_idx.min()
+        x_idx_max = x_idx.max()
+        _mask_bound = np.full(_mask.shape, False)
+        _mask_bound[y_idx_min:y_idx_max + 1, x_idx_min:x_idx_max + 1] = True
+
+        coords = {ds_dims[0]: ds.coords[ds_dims[0]], ds_dims[1]: ds.coords[ds_dims[1]]}
+        mask = xr.DataArray(_mask, coords, dims=ds_dims)
+        mask_bound = xr.DataArray(_mask_bound, coords, dims=ds_dims)
+
+        ds_masked = ds.where(mask, drop=True)
+        ds_masked.attrs["transform"] = transform
+        ds_masked.attrs["bounds"] = _geometry.bounds
+
+        ds_bound_masked = ds.where(mask_bound, drop=True)
+        ds_bound_masked.attrs["transform"] = transform
+        ds_bound_masked.attrs["bounds"] = _geometry.bounds
+
+        arr_lat = ds_masked['lat'].values.flatten()
+        arr_lon = ds_masked['lon'].values.flatten()
+        arr_data = ds_masked['prcp'].values[0, :, :].flatten()
+
+        arr_all = np.c_[arr_lat, arr_lon, arr_data]
+        # remove the rows with nan value
+        arr = arr_all[~np.isnan(arr_all).any(axis=1)]
+        df = pd.DataFrame(data=arr, columns=['lat', 'lon', 'prcp'])
+        df.to_csv(os.path.join(self.save_dir, 'geometry_load_to_qgis.csv'), index=False)
+
+        arr_bound_lat = ds_bound_masked['lat'].values.flatten()
+        arr_bound_lon = ds_bound_masked['lon'].values.flatten()
+        arr_bound_data = ds_bound_masked['prcp'].values[0, :, :].flatten()
+
+        arr_bound_all = np.c_[arr_bound_lat, arr_bound_lon, arr_bound_data]
+        # remove the rows with nan value
+        arr_bound = arr_bound_all[~np.isnan(arr_bound_all).any(axis=1)]
+        df_bound = pd.DataFrame(data=arr_bound, columns=['lat', 'lon', 'prcp'])
+        df_bound.to_csv(os.path.join(self.save_dir, 'bound_load_to_qgis.csv'), index=False)
+        # after getting the csv file, please use "Layer -> Add Layer -> Add Delimited Text Layer" in QGIS to import it.
 
 
 if __name__ == '__main__':
