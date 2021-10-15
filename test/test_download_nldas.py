7b0ecfd646dc4904e81ae314b966ce303d09f85d
Wenyu Ouyang <hust2014owen@gmail.com>
2021-10-15 17:02:03 +0800

    add nldas download func

---
+++ test/test_download_nldas.py
@@ -0,0 +1,18 @@
+import os.path
+import unittest
+
+import definitions
+from hydrobench.nldas4basins.download_nldas import download_nldas_with_url_lst
+
+
+class MyTestCase(unittest.TestCase):
+    def test_download_nldas_hourly(self):
+        url_lst_file = os.path.join(definitions.ROOT_DIR, "hydrobench", "nldas4basins",
+                                    "subset_NLDAS_FORA0125_H_002_20211015_083206.txt")
+        save_dir = os.path.join(definitions.DATASET_DIR, "nldas_hourly")
+        download_nldas_with_url_lst(url_lst_file, save_dir)
+        print("Downloading NLDAS hourly data")
+
+
+if __name__ == '__main__':
+    unittest.main()
