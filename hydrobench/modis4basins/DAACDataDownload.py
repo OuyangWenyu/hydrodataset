e35b2b30fda035e5cfe87c422fc7c2dd3e4c1f5d
Owen <ouyangwenyu@mail.dlut.edu.cn>
2021-10-18 16:53:26 +0800

    modis downloading

--- hydrobench/modis4basins/DAACDataDownload.py
+++ hydrobench/modis4basins/DAACDataDownload.py
@@ -21,11 +21,13 @@ import requests
 # Set up command line arguments
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-dir', '--directory', required=True, help='Specify directory to save files to')
-parser.add_argument('-f', '--files', required=True, help='A single granule URL, or the location of csv or textfile containing granule URLs')
+parser.add_argument('-f', '--files', required=True,
+                    help='A single granule URL, or the location of csv or textfile containing granule URLs')
 args = parser.parse_args()
 
 saveDir = args.directory  # Set local directory to download to
-files = args.files        # Define file(s) to download from the LP DAAC Data Pool
+files = args.files  # Define file(s) to download from the LP DAAC Data Pool
+
 prompts = ['Enter NASA Earthdata Login Username \n(or create an account at urs.earthdata.nasa.gov): ',
            'Enter NASA Earthdata Login Password: ']
 
@@ -34,23 +36,24 @@ prompts = ['Enter NASA Earthdata Login Username \n(or create an account at urs.e
 if files.endswith('.txt') or files.endswith('.csv'):
     fileList = open(files, 'r').readlines()  # If input is textfile w file URLs
 elif isinstance(files, str):
-    fileList = [files]                       # If input is a single file
+    fileList = [files]  # If input is a single file
 
 # Generalize download directory
 if saveDir[-1] != '/' and saveDir[-1] != '\\':
     saveDir = saveDir.strip("'").strip('"') + os.sep
-urs = 'urs.earthdata.nasa.gov'    # Address to call for authentication
+urs = 'urs.earthdata.nasa.gov'  # Address to call for authentication
 
 # --------------------------------AUTHENTICATION CONFIGURATION----------------------------------- #
 # Determine if netrc file exists, and if so, if it includes NASA Earthdata Login Credentials
 try:
-    netrcDir = os.path.expanduser("~/.netrc")
+    netrcDir = os.path.expanduser(os.path.join("~", ".netrc"))
     netrc(netrcDir).authenticators(urs)[0]
 
 # Below, create a netrc file and prompt user for NASA Earthdata Login Username and Password
 except FileNotFoundError:
     homeDir = os.path.expanduser("~")
-    Popen('touch {0}.netrc | chmod og-rw {0}.netrc | echo machine {1} >> {0}.netrc'.format(homeDir + os.sep, urs), shell=True)
+    Popen('touch {0}.netrc | chmod og-rw {0}.netrc | echo machine {1} >> {0}.netrc'.format(homeDir + os.sep, urs),
+          shell=True)
     Popen('echo login {} >> {}.netrc'.format(getpass(prompt=prompts[0]), homeDir + os.sep), shell=True)
     Popen('echo password {} >> {}.netrc'.format(getpass(prompt=prompts[1]), homeDir + os.sep), shell=True)
 
@@ -76,11 +79,14 @@ for f in fileList:
     if not os.path.exists(saveDir):
         os.makedirs(saveDir)
     saveName = os.path.join(saveDir, f.split('/')[-1].strip())
-
+    if os.path.isfile(saveName):
+        continue
     # Create and submit request and download file
-    with requests.get(f.strip(), verify=False, stream=True, auth=(netrc(netrcDir).authenticators(urs)[0], netrc(netrcDir).authenticators(urs)[2])) as response:
+    with requests.get(f.strip(), verify=False, stream=True, auth=(
+            netrc(netrcDir).authenticators(urs)[0], netrc(netrcDir).authenticators(urs)[2])) as response:
         if response.status_code != 200:
-            print("{} not downloaded. Verify that your username and password are correct in {}".format(f.split('/')[-1].strip(), netrcDir))
+            print("{} not downloaded. Verify that your username and password are correct in {}".format(
+                f.split('/')[-1].strip(), netrcDir))
         else:
             response.raw.decode_content = True
             content = response.raw
@@ -91,4 +97,4 @@ for f in fileList:
                         break
                     d.write(chunk)
             print('Downloaded file: {}'.format(saveName))
-    #time.sleep(1.0)
+    # time.sleep(1.0)
