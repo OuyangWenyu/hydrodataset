b0277527d907d200c62769ed34149fec096803c5
owen <hust2014owen@gmail.com>
2022-09-06 14:23:17 +0800

    try github actions

--- hydrodataset/hydrodataset.py
+++ hydrodataset/hydrodataset.py
@@ -1,7 +1,7 @@
 """
 Author: Wenyu Ouyang
 Date: 2022-09-05 23:20:24
-LastEditTime: 2022-09-06 09:43:17
+LastEditTime: 2022-09-06 14:21:36
 LastEditors: Wenyu Ouyang
 Description: main modules for hydrodataset
 FilePath: \hydrodataset\hydrodataset\hydrodataset.py
@@ -9,3 +9,6 @@ Copyright (c) 2021-2022 Wenyu Ouyang. All rights reserved.
 """
 def get_hydrodatasets():
     return ["CAMELS-US"]
+
+def get_hydrodatasets_description():
+    return ["CAMELS dataset (only for U.S.)"]
