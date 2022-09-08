3231da7d369ab997bec0b4382b805cf3a35b31b7
owen <hust2014owen@gmail.com>
2022-09-08 19:46:58 +0800

    Bump version: 0.0.2 → 0.0.3

--- hydrodataset/__init__.py
+++ hydrodataset/__init__.py
@@ -12,7 +12,7 @@ import os
 
 __author__ = """Wenyu Ouyang"""
 __email__ = "wenyuouyang@outlook.com"
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 # we use a .hydrodataset dir to save the setting
 # more file/dir operations could be seen in:
