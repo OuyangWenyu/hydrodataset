7c74308676fbb5974da03b219767cf334893b0a6
ouyangwenyu <wenyuouyang@outlook.com>
2023-07-26 17:55:43 +0800

    update setup.py file to fix gbk error

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
-"""The setup script."""
+"""
+Author: Wenyu Ouyang
+Date: 2022-09-05 23:22:54
+LastEditTime: 2023-07-26 17:54:32
+LastEditors: Wenyu Ouyang
+Description: The setup script
+FilePath: \hydrodataset\setup.py
+Copyright (c) 2023-2024 Wenyu Ouyang. All rights reserved.
+"""
 
 import io
+import pathlib
 from os import path as op
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md", "r", encoding="utf-8") as readme_file:
     readme = readme_file.read()
-
 here = op.abspath(op.dirname(__file__))
+
 
 # get the dependencies and installs
 with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
@@ -18,40 +27,44 @@ with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
 install_requires = [x.strip() for x in all_reqs if "git+" not in x]
 dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Wenyu Ouyang",
-    author_email='wenyuouyang@outlook.com',
-    python_requires='>=3.7',
+    author_email="wenyuouyang@outlook.com",
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="A Python package for downloading and reading hydrological datasets",
     install_requires=install_requires,
     dependency_links=dependency_links,
     license="MIT license",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords='hydrodataset',
-    name='hydrodataset',
-    packages=find_packages(include=['hydrodataset', 'hydrodataset.*']),
+    keywords="hydrodataset",
+    name="hydrodataset",
+    packages=find_packages(include=["hydrodataset", "hydrodataset.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/OuyangWenyu/hydrodataset',
-    version='0.1.0',
+    url="https://github.com/OuyangWenyu/hydrodataset",
+    version="0.1.0",
     zip_safe=False,
 )
