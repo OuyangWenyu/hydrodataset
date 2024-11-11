35154e98ed347da3573d0925e25c05d6949e9c74
EstelleMing <1714424170@qq.com>
2024-11-11 16:30:54 +0800

    Added CAMELS_AUS_v2.0.0 data; Modify the calculation of the function unit of read_target_cols() --only streamflow_MLd; Added unit selection to read_mean_prcp().

--- examples/scripts.py
+++ examples/scripts.py
@@ -18,7 +18,9 @@ camels_br_path = os.path.join("camels", "camels_br")
 camels_cl_path = os.path.join("camels", "camels_cl")
 camels_gb_path = os.path.join("camels", "camels_gb")
 camels_us_path = os.path.join("camels", "camels_us")
+camels_aus_v2_path=os.path.join("camels","camels_aus_v2")
 
+aus_v2_region = "AUS_v2"
 aus_region = "AUS"
 br_region = "BR"
 cl_region = "CL"
@@ -170,6 +172,33 @@ attr_types = camels_aus.get_constant_cols()
 np.testing.assert_array_equal(
     attr_types[:3], np.array(["station_name", "drainage_division", "river_region"])
 )
+
+# # ---------------------------- AUS-V2 -------------------------------
+camels_aus_v2=Camels(camels_aus_v2_path, download=False, region=aus_v2_region)
+gage_ids = camels_aus_v2.read_object_ids()
+assert gage_ids.size == 561
+attrs = camels_aus_v2.read_constant_cols(
+    gage_ids[:5], var_lst=["catchment_area", "geol_sec", "metamorph"]
+)
+print(attrs)
+forcings = camels_aus_v2.read_relevant_cols(
+    gage_ids[:5],
+    ["1990-01-01", "2010-01-01"],
+    var_lst=["precipitation_AGCD", "et_morton_actual_SILO", "tmin_SILO"],
+)
+print(forcings.shape)
+flows = camels_aus_v2.read_target_cols(
+    gage_ids[:5],
+    ["1990-01-01", "2010-01-01"],
+    target_cols=["streamflow_MLd", "streamflow_mmd"],
+)
+print(flows.shape)
+streamflow_types = camels_aus_v2.get_target_cols()
+print(streamflow_types)
+focing_types = camels_aus_v2.get_relevant_cols()
+print(focing_types)
+attr_types = camels_aus_v2.get_constant_cols()
+print(attr_types)
 
 # ------------------------------ BR --------------------------------
 camels_br = Camels(camels_br_path, download=False, region=br_region)
