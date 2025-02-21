41fb4aabfe3383c64cbac67d2cb234a7b88f8069
shouz <thetempmail@163.com>
2025-02-21 20:31:03 +0800

    add cache forcing and streamflow, attributes,xrdataset

--- hydrodataset/camels_fr.py
+++ hydrodataset/camels_fr.py
@@ -10,6 +10,8 @@ from hydroutils import hydro_time, hydro_file
 from hydrodataset import HydroDataset, CACHE_DIR, CAMELS_REGIONS
 from hydrodataset.camels import Camels, time_intersect_dynamic_data
 from pandas.api.types import is_string_dtype, is_numeric_dtype
+import json
+import warnings
 
 CAMELS_NO_DATASET_ERROR_LOG = (
     "We cannot read this dataset now. Please check if you choose correctly:\n"
@@ -189,7 +191,7 @@ class CamelsFr(Camels):
         gage_id
             the station id
         t_range
-            the time range, for example, ["19700101", "20211231"]
+            the time range, for example, ["1970-01-01", "2021-12-31"]
         var_type
             flow type: "tsd_q_l", "tsd_q_mm"
             forcing type: "tsd_prec","tsd_prec_solid_frac","tsd_temp","tsd_pet_ou","tsd_pet_pe","tsd_pet_pm","tsd_wind",
@@ -230,7 +232,7 @@ class CamelsFr(Camels):
         gage_id_lst
             station ids
         t_range
-            the time range, for example, ["19700101", "20211231"]
+            the time range, for example, ["1970-01-01", "2021-12-31"]
         target_cols
             the default is None, but we need at least one default target.
             For CAMELS-FR, it's ["tsd_q_l"]
@@ -278,7 +280,7 @@ class CamelsFr(Camels):
         gage_id_lst
             station ids
         t_range
-            the time range, for example, ["19700101", "20211231"]
+            the time range, for example, ["1970-01-01", "2021-12-31"]
         var_lst
             forcing variable type: "tsd_prec","tsd_prec_solid_frac","tsd_temp","tsd_pet_ou","tsd_pet_pe","tsd_pet_pm","tsd_wind",
             "tsd_humid","tsd_rad_dli","tsd_rad_ssi","tsd_swi_gr","tsd_swi_isba","tsd_swe_isba","tsd_temp_min","tsd_temp_max"
@@ -403,3 +405,226 @@ class CamelsFr(Camels):
                 "unit must be one of ['mm/d', 'mm/day', 'mm/h', 'mm/hour', 'mm/3h', 'mm/3hour', 'mm/8d', 'mm/8day']"
             )
         return converted_data
+
+    def cache_forcing_np_json(self):
+        """
+        Save all basin-forcing data in a numpy array file in the cache directory.
+
+        Because it takes much time to read data from csv files,
+        it is a good way to cache data as a numpy file to speed up the reading.
+        In addition, we need a document to explain the meaning of all dimensions.
+
+        """
+        cache_npy_file = CACHE_DIR.joinpath("camels_fr_forcing.npy")
+        json_file = CACHE_DIR.joinpath("camels_fr_forcing.json")
+        variables = self.get_relevant_cols()
+        basins = self.sites["sta_code_h3"].values
+        t_range = ["1970-01-01", "2021-12-31"]
+        times = [
+            hydro_time.t2str(tmp)
+            for tmp in hydro_time.t_range_days(t_range).tolist()
+        ]
+        data_info = collections.OrderedDict(
+            {
+                "dim": ["basin", "time", "variable"],
+                "basin": basins.tolist(),
+                "time": times,
+                "variable": variables.tolist(),
+            }
+        )
+        with open(json_file, "w") as FP:
+            json.dump(data_info, FP, indent=4)
+        data = self.read_relevant_cols(
+            gage_id_lst=basins.tolist(),
+            t_range=t_range,
+            var_lst=variables.tolist(),
+        )
+        np.save(cache_npy_file, data)
+
+    def cache_streamflow_np_json(self):
+        """
+        Save all basins' streamflow data in a numpy array file in the cache directory
+        """
+        cache_npy_file = CACHE_DIR.joinpath("camels_fr_streamflow.npy")
+        json_file = CACHE_DIR.joinpath("camels_fr_streamflow.json")
+        variables = self.get_target_cols()
+        basins = self.sites["sta_code_h3"].values
+        t_range = ["1970-01-01", "2021-12-31"]
+        times = [
+            hydro_time.t2str(tmp) for tmp in hydro_time.t_range_days(t_range).tolist()
+        ]
+        data_info = collections.OrderedDict(
+            {
+                "dim": ["basin", "time", "variable"],
+                "basin": basins.tolist(),
+                "time": times,
+                "variable": variables.tolist(),
+            }
+        )
+        with open(json_file, "w") as FP:
+            json.dump(data_info, FP, indent=4)
+        data = self.read_target_cols(
+            gage_id_lst=basins,
+            t_range=t_range,
+            target_cols=variables,
+        )
+        np.save(cache_npy_file, data)
+
+    def cache_attributes_xrdataset(self):
+        """Convert all the attributes to a single dataframe
+
+        Returns
+        -------
+        None
+        """
+        # NOTICE: although it seems that we don't use pint_xarray, we have to import this package
+        import pint_xarray
+
+        attr_files = self.data_source_dir.glob("CAMELS_FR_*.csv")
+        attrs = {
+            f.stem.split("_")[1]: pd.read_csv(
+                f, sep=",", index_col=0, dtype={"sta_code_h3": str}
+            )
+            for f in attr_files
+        }
+
+        # attrs_df = pd.concat(attrs.values(), axis=1)
+        attrs_df = attrs
+
+        # fix station names
+        def fix_station_nm(station_nm):
+            name = station_nm.title().rsplit(" ", 1)
+            name[0] = name[0] if name[0][-1] == "," else f"{name[0]},"
+            name[1] = name[1].replace(".", "")
+            return " ".join(
+                (name[0], name[1].upper() if len(name[1]) == 2 else name[1].title())
+            )
+
+        attrs_df["gauge_name"] = [fix_station_nm(n) for n in attrs_df["gauge_name"]]
+        obj_cols = attrs_df.columns[attrs_df.dtypes == "object"]
+        for c in obj_cols:
+            attrs_df[c] = attrs_df[c].str.strip().astype(str)
+
+        # transform categorical variables to numeric
+        categorical_mappings = {}
+        for column in attrs_df.columns:
+            if attrs_df[column].dtype == "object":
+                attrs_df[column] = attrs_df[column].astype("category")
+                categorical_mappings[column] = dict(
+                    enumerate(attrs_df[column].cat.categories)
+                )
+                attrs_df[column] = attrs_df[column].cat.codes
+
+        # unify id to basin
+        attrs_df.index.name = "basin"
+        # We use xarray dataset to cache all data
+        ds_from_df = attrs_df.to_xarray()
+        units_dict = {      # todo:
+            "gauge_lat": "degree",
+            "gauge_lon": "degree",
+            "elev_mean": "m",
+            "slope_mean": "m/km",
+
+        }
+
+        # Assign units to the variables in the Dataset
+        for var_name in units_dict:
+            if var_name in ds_from_df.data_vars:
+                ds_from_df[var_name].attrs["units"] = units_dict[var_name]
+
+        # Assign categorical mappings to the variables in the Dataset
+        for column in ds_from_df.data_vars:
+            if column in categorical_mappings:
+                mapping_str = categorical_mappings[column]
+                ds_from_df[column].attrs["category_mapping"] = str(mapping_str)
+        return ds_from_df
+
+    def cache_forcing_xrdataset(self):
+        """Save all basin-forcing data in a netcdf file in the cache directory.
+
+        """
+        cache_npy_file = CACHE_DIR.joinpath("camels_fr_forcing.npy")
+        json_file = CACHE_DIR.joinpath("camels_fr_forcing.json")
+        if (not os.path.isfile(cache_npy_file)) or (not os.path.isfile(json_file)):
+            self.cache_forcing_np_json()
+        forcing = np.load(cache_npy_file)
+        with open(json_file, "r") as fp:
+            forcing_dict = json.load(
+                fp, object_pairs_hook=collections.OrderedDict
+            )
+        import pint_xarray
+
+        basins = forcing_dict["basin"]
+        times = pd.date_range(
+            forcing_dict["time"][0], periods=len(forcing_dict["time"])
+        )
+        variables = forcing_dict["variable"]
+
+        units = ["s", "mm/day", "W/m^2", "mm", "°C", "°C", "Pa", "mm/day"]
+        return xr.Dataset(
+            data_vars={
+                **{
+                    variables[i]: (
+                        ["basin", "time"],
+                        forcing[:, :, i],
+                        {"units": units[i]},
+                    )
+                    for i in range(len(variables))
+                }
+            },
+            coords={
+                "basin": basins,
+                "time": times,
+            },
+            attrs={"forcing_type": ""},
+        )
+
+    def cache_streamflow_xrdataset(self):
+        """Save all basins' streamflow data in a netcdf file in the cache directory
+
+        """
+        cache_npy_file = CACHE_DIR.joinpath("camels_fr_streamflow.npy")
+        json_file = CACHE_DIR.joinpath("camels_fr_streamflow.json")
+        if (not os.path.isfile(cache_npy_file)) or (not os.path.isfile(json_file)):
+            self.cache_streamflow_np_json()
+        streamflow = np.load(cache_npy_file)
+        with open(json_file, "r") as fp:
+            streamflow_dict = json.load(fp, object_pairs_hook=collections.OrderedDict)
+        import pint_xarray
+
+        basins = streamflow_dict["basin"]
+        times = pd.date_range(
+            streamflow_dict["time"][0], periods=len(streamflow_dict["time"])
+        )
+        return xr.Dataset(
+            {
+                "streamflow": (
+                    ["basin", "time"],
+                    streamflow[:, :, 0],
+                    {"units": self.streamflow_unit},
+                ),
+                "ET": (
+                    ["basin", "time"],
+                    streamflow[:, :, 1],
+                    {"units": "mm/day"},
+                ),
+            },
+            coords={
+                "basin": basins,
+                "time": times,
+            },
+        )
+
+    def cache_xrdataset(self):
+        """
+        Save all data in a netcdf file in the cache directory
+
+        """
+
+        warnings.warn("Check you units of all variables")
+        ds_attr = self.cache_attributes_xrdataset()
+        ds_attr.to_netcdf(CACHE_DIR.joinpath("camelsfr_attributes.nc"))
+        ds_streamflow = self.cache_streamflow_xrdataset()
+        ds_forcing = self.cache_forcing_xrdataset()
+        ds = xr.merge([ds_streamflow, ds_forcing])
+        ds.to_netcdf(CACHE_DIR.joinpath("camelsfr_timeseries.nc"))
