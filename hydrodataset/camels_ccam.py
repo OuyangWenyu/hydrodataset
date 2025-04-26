66e0a377fb30b446292257b8e5ae2667c5fa0f53
shouz <thetempmail@163.com>
2025-04-26 17:16:06 +0800

    add get_constant_cols, read_gage_flow_forcing and read_target_clos.

--- hydrodataset/camels_ccam.py
+++ hydrodataset/camels_ccam.py
@@ -18,19 +18,19 @@ CAMELS_NO_DATASET_ERROR_LOG = (
 
 camelsccam_arg = {
     "forcing_type": "observation",
-    "gauge_id_tag": "gauge_id",
+    "gauge_id_tag": "basin_id",
     "area_tag": ["area", ],
-    "meanprcp_unit_tag": [["p_mean"], "mm/d"],
+    "meanprcp_unit_tag": [["pre_mean"], "mm/d"],
     "time_range": {
-        "observation": ["1981-01-01","2021-01-01"],
+        "observation": ["1990-01-01","2021-04-01"],
     },
-    "target_cols": ["discharge_vol(m3/s)", "discharge_spec(mm/d)"],
+    "target_cols": ["q", ],
     "b_nestedness": False,
     "forcing_unit": ["m", "mm/day", "°C", "°C", "°C", "%", "mm"],
     "data_file_attr": {
         "sep": ",",
         "header": 1,
-        "attr_file_str": ["CAMELS_CH_", "_attributes.csv", "_attributes_obs.csv", ]
+        "attr_file_str": ["2_", "_attributes.csv", "_attributes_obs.csv", ]
     },
 }
 
@@ -89,10 +89,19 @@ class CamelsCcam(Camels):
         ]
         gauge_id_file = attr_dir.joinpath("CAMELS_CCAM_hydrology_attributes_obs.csv")
         nestedness_information_file = None
-        base_url = "https://zenodo.org/records/15025258"
+        base_url = "https://zenodo.org/records/5729444"
         download_url_lst = [
-            f"{base_url}/files/camels_ccam.zip",
-            f"{base_url}/files/camels_ccam_data_description.pdf",
+            f"{base_url}/files/0_catchment_boundary.zip",
+            f"{base_url}/files/1_meteorological.zip",
+            f"{base_url}/files/2_location_topography.txt",
+            f"{base_url}/files/3_land_cover_characteristics.txt",
+            f"{base_url}/files/4_geological_characteristics.txt",
+            f"{base_url}/files/5_climate_indices.txt",
+            f"{base_url}/files/6_soil.txt",
+            f"{base_url}/files/7_HydroMLYR.zip",
+            f"{base_url}/files/8_attribute_descriptions.xlsx",
+            f"{base_url}/files/9_code_data.zip",
+            f"{base_url}/files/readme.txt",
         ]
 
         return collections.OrderedDict(
@@ -106,3 +115,119 @@ class CamelsCcam(Camels):
             CAMELS_BASINS_SHP=camels_shp_file,
             CAMELS_DOWNLOAD_URL_LST=download_url_lst,
         )
+
+    def get_constant_cols(self) -> np.ndarray:
+        """
+        all readable attrs in CAMELS-CH
+
+        Returns
+        -------
+        np.ndarray
+            attribute types
+        """
+        data_folder = self.data_source_description["CAMELS_ATTR_DIR"]
+        return self._get_constant_cols_some(
+            data_folder, "2_","_attributes_obs.csv",","
+        )
+
+    def get_relevant_cols(self) -> np.ndarray:
+        """
+        all readable forcing types in CAMELS-CH
+
+        Returns
+        -------
+        np.ndarray
+            forcing types
+        """
+        return np.array(
+            [
+                "waterlevel(m)",
+                "precipitation(mm/d)",
+                "temperature_min(degC)",
+                "temperature_mean(degC)",
+                "temperature_max(degC)",
+                "rel_sun_dur(%)",
+                "swe(mm)",
+            ]
+        )
+
+    def read_ccam_gage_flow_forcing(self, gage_id, t_range, var_type):
+        """
+        Read gage's streamflow or forcing from CAMELS-CCAM
+
+        Parameters
+        ----------
+        gage_id
+            the station id
+        t_range
+            the time range, for example, ["1981-01-01","2021-01-01"]
+        var_type
+            flow type: "discharge_vol(m3/s)", "discharge_spec(mm/d)"
+            forcing type: "waterlevel(m)", "precipitation(mm/d)", "temperature_min(degC)", "temperature_mean(degC)", "temperature_max(degC)", "rel_sun_dur(%)", "swe(mm)"
+
+        Returns
+        -------
+        np.array
+            streamflow or forcing data of one station for a given time range
+        """
+        logging.debug("reading %s streamflow data", gage_id)
+        gage_file = os.path.join(
+            self.data_source_description["CAMELS_FLOW_DIR"],
+            "CAMELS_CCAM_obs_based_" + gage_id + ".csv",
+        )
+        data_temp = pd.read_csv(gage_file, sep=self.data_file_attr["sep"])
+        obs = data_temp[var_type].values
+        if var_type in self.target_cols:
+            obs[obs < 0] = np.nan
+        date = pd.to_datetime(data_temp["date"]).values.astype("datetime64[D]")
+        return time_intersect_dynamic_data(obs, date, t_range)
+
+    def read_target_cols(
+        self,
+        gage_id_lst: Union[list, np.array] = None,
+        t_range: list = None,
+        target_cols: Union[list, np.array] = None,
+        **kwargs,
+    ) -> np.ndarray:
+        """
+        read target values. for CAMELS-CCAM, they are streamflows.
+
+        default target_cols is an one-value list
+        Notice, the unit of target outputs in different regions are not totally same
+
+        Parameters
+        ----------
+        gage_id_lst
+            station ids
+        t_range
+            the time range, for example, ["1981-01-01","2021-01-01"]
+        target_cols
+            the default is None, but we need at least one default target.
+            For CAMELS-CCAM, it's ["q"]
+        kwargs
+            some other params if needed
+
+        Returns
+        -------
+        np.array
+            streamflow data, 3-dim [station, time, streamflow]
+        """
+        if target_cols is None:
+            return np.array([])
+        else:
+            nf = len(target_cols)
+        t_range_list = hydro_time.t_range_days(t_range)
+        nt = t_range_list.shape[0]
+        y = np.full([len(gage_id_lst), nt, nf], np.nan)
+        for j in tqdm(
+            range(len(target_cols)), desc="Read streamflow data of CAMELS-CCAM"
+        ):
+            for k in tqdm(range(len(gage_id_lst))):
+                data_obs = self.read_ch_gage_flow_forcing(
+                    gage_id_lst[k], t_range, target_cols[j]
+                )
+                y[k, :, j] = data_obs
+        # Keep unit of streamflow unified: we use ft3/s here
+        # other units are m3/s -> ft3/s
+        y = self.unit_convert_streamflow_m3tofoot3(y)
+        return y
